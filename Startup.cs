5dbf17400c880e0d6f3b2155d183a15e662246ba
Diego Palomino <dpalominobeltran@outlook.com>
2020-04-22 14:53:36 -0400

    Add swagger to the project

--- Startup.cs
+++ Startup.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
 using Soccer.Web.Data;
 using SoccerWebApi.Data;
 using SoccerWebApi.Helpers;
@@ -44,6 +38,25 @@ namespace SoccerWebApi
             services.AddScoped<IMailHelper, MailHelper>();
             services.AddScoped<IMatchHelper, MatchHelper>();
             services.AddScoped<IBlobHelper, BlobHelper>();
+
+            // Register the Swagger generator, defining one or more Swagger documents
+            services.AddSwaggerGen(swagger =>
+            {
+                swagger.SwaggerDoc(
+                    SwaggerConfiguration.DocNameV1,
+                        new OpenApiInfo
+                        {
+                            Title = SwaggerConfiguration.DocInfoTitle,
+                            Version = SwaggerConfiguration.DocInfoVersion,
+                            Description = SwaggerConfiguration.DocInfoDescription,
+                            Contact = new OpenApiContact()
+                            {
+                                Name = SwaggerConfiguration.ContactName,
+                                Email = SwaggerConfiguration.Email
+                            }
+                        }
+                );
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -57,6 +70,15 @@ namespace SoccerWebApi
             {
                 app.UseHsts();
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint(SwaggerConfiguration.EndpointUrl, SwaggerConfiguration.EndpointDescription);
+            });
 
             app.UseHttpsRedirection();
             app.UseMvc();
---
+++ SwaggerConfiguration.cs
@@ -0,0 +1,44 @@
+﻿public class SwaggerConfiguration
+{
+
+    /// <summary>
+    /// <para>Foo API v1</para>
+    /// </summary>
+    public const string EndpointDescription = "SoccerWeb API v1";
+
+    /// <summary>
+    /// <para>/swagger/v1/swagger.json</para>
+    /// </summary>
+    public const string EndpointUrl = "/swagger/v1/swagger.json";
+
+    /// <summary>
+    /// <para>Jorge Serrano</para>
+    /// </summary>
+    public const string ContactName = "Diego Palomino";
+
+    /// <summary>
+    /// <para>diego.palomino@fujitsu.com</para>
+    /// </summary>
+    public const string Email = "diego.palomino@fujitsu.com";
+
+    /// <summary>
+    /// <para>v1</para>
+    /// </summary>
+    public const string DocNameV1 = "v1";
+
+    /// <summary>
+    /// <para>Foo API</para>
+    /// </summary>
+    public const string DocInfoTitle = "SoccerWeb API";
+
+    /// <summary>
+    /// <para>v1</para>
+    /// </summary>
+    public const string DocInfoVersion = "v1";
+
+    /// <summary>
+    /// <para>Foo Api - Sample Web API in ASP.NET Core 2</para>
+    /// </summary>
+    public const string DocInfoDescription = "SoccerWeb Api - Sample Web API in ASP.NET Core 2.1";
+
+}
